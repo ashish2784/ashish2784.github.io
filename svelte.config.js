660b299ae2acc7cc40ffae8a4a0a323c334a5d2a
Ashish <ashishmanoj3015@example.com>
2025-12-31 12:30:58 +0530

    Setup GitHub Pages deployment

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -8,7 +8,15 @@ const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
-		adapter: adapter(),
+		adapter: adapter({
+			// default options are shown. On some platforms
+			// these options are set automatically — see below
+			pages: 'build',
+			assets: 'build',
+			fallback: '404.html',
+			precompress: false,
+			strict: true
+		}),
 		csp: {
 			directives: {
 				'script-src': ['self', 'unsafe-inline', 'https://www.gstatic.com', 'https://dialogflow.cloud.google.com'],
